edf912ee9b0a0a30b2e91d072fe5a5e58598655e
hadjal_d@yahoo.fr <hadjal_d@yahoo.fr>
2021-09-21 17:31:00 -0400

    added files completed

---
+++ src/basic20/HelloWorld20.java
@@ -0,0 +1,41 @@
+package basic20;
+
+public class HelloWorld20 { // class body start point
+	
+	// create a java class
+	// between this curly braces is called class body
+	
+	// Class body: for code writing purpose
+	// In class we can create variables, methods
+	//. Java is extension of a class
+	// 
+	
+	
+	// Main method: Driver method or executable method
+	
+	static String firstname="Iddir";
+	
+	public static void main (String args[]) { // method body start point
+		
+		
+		System.out.println("Welcome to java class");
+		System.out.println("Hi Iddir Hadjal");
+		
+		// when we run our code actually JVM execute main method
+		// Inside double quote "khdsgfgshfgsjfg" called String 
+		
+		
+		
+		
+	} // method body end point 
+ 	
+	
+	
+	
+	
+
+	
+	
+	
+
+} // class body end point
---
+++ src/hello20/Computer.java
@@ -0,0 +1,21 @@
+package hello20;
+
+public class Computer {
+	
+	
+	public static void main (String args[]) {
+		
+		
+		System.out.println("This is computer class");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
